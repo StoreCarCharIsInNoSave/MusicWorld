239b7090f886a716744ddb579043adc17420dd43
unknown <anonvtrede@gmail.com>
2022-01-14 18:13:44 +0300

    Created admin system for user model

---
+++ db/migrate/20220114151107_is_admin_to_user_table.rb
@@ -0,0 +1,5 @@
+class IsAdminToUserTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :is_admin, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_141327) do
+ActiveRecord::Schema.define(version: 2022_01_14_151107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_141327) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
