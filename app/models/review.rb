af41c357b695041fbfe1980f8e3f66b898aeaccf
StoreCarCharIsInNoSave <anonvtrede@gmail.com>
2022-01-28 16:40:24 +0300

    Some fixes\

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
   validates :mark, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :comment, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :comment, presence: true, length: { minimum: 5, maximum: 100 }
 end
